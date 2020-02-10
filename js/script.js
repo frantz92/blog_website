1d588d9b93fd1a4e78a1a3446b847386cca46643
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-02-10 14:31:29 +0100

    Add click event and action for authors

--- js/script.js
+++ js/script.js
@@ -3,33 +3,25 @@
 function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
-
   /*  [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-
   /* [DONE] add class 'active' to the clicked link */
   //console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
-
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
-
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-
   /* [DONE] get 'href' attribute from the clicked link */
   let referenceLink = clickedElement.getAttribute('href');
   //console.log(referenceLink);
-
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const choosenArticle = document.querySelector(referenceLink);
   //console.log(choosenArticle);
-
   /* [DONE] add class 'active' to the correct article */
   choosenArticle.classList.add('active');
 }
@@ -168,3 +160,19 @@ function generateAuthor(){
   }
 }
 generateAuthor();
+
+function authorClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
+  let author = clickedElement.textContent;
+  console.log(author);
+  generateTitleLinks('[data-author="' + author + '"]');
+}
+
+function addClickListenersToAuthor(){
+  const links = document.querySelectorAll(optArticleAuthorSelector);
+  for(let link of links){
+    link.addEventListener('click', authorClickHandler);
+  }
+}
+addClickListenersToAuthor();
